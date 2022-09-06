5a211b45ce1a64fa0fcde2ad8f23e80d979de97f
Soba4kaxbm <Soba4kaxbm@mail.ru>
2022-09-06 21:18:36 +0600

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./nav";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Nav />
+      <div>Create React App Without CRA😊</div>
+      <button>Click Me 😎</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/nav.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+   {
+    width: 100px;
+    height: 50px;
+    background-color: red;
+    color: white;
+  }
+`;
+
+const Nav: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Bay</Button>
+    </div>
+  );
+};
+
+export default Nav;
